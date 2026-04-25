5ee121e692bec46496eb0a51cb18c278079f2281
Siddhi Singh <siddhisingh18102005@gmail.com>
2026-04-25 18:13:21 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ import Services from "@/components/Services";
 import Start from "@/components/Start";
 import Team from "@/components/Team";
 import WhoWeAre from "./about/whoWeAre/page";
-import { Testimonials } from "./testimonials/page";
 import AchievementSection from "./achievements/page";
+import Testimonials from "./testimonials/page";
 // import { ContactIcon } from "@/components/ContactIcon";
 
 export default function Home() {
